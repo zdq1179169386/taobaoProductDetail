0d37fd6963569e8fb9b18ef488503edc72548ee3
zhl1179169386 <2259434901@qq.com>
2016-01-20 11:34:52 +0800

    GitHub 不给用中文
    GitHub 不给用中文，pod下来的MJRefresh
    是新版本，不知道什么鬼，自定义的头视图不能用了，self.tableView.footer 也换成
    self.tableView.mj_footer

--- 淘宝详情的分屏显示/淘宝详情的分屏显示/ZDQ_ViewController.h
+++ 淘宝详情的分屏显示/淘宝详情的分屏显示/ZDQ_ViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by yb on 16/1/20.
 //  Copyright © 2016年 yb. All rights reserved.
-//
+// GitHub 不给用中文，pod下来的MJRefresh 是新版本，不知道什么鬼，自定义的头视图不能用了，self.tableView.footer 也换成 self.tableView.mj_footer
 
 #import <UIKit/UIKit.h>
 
